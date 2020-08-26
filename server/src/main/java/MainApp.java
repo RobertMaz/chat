3f6d68fea0e4061c6e30727e6c436d7f54af72e5
MyComp <maz.robert@mail.ru>
2020-08-26 18:28:40 +0300

    chatGBCreated

---
+++ server/src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8181)) {
+            System.out.println("Server started");
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            int in = socket.getInputStream().read();
+
+            System.out.print((char) in);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
